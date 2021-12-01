45b8815e08301c20a9a6b00b82bdb75383c80ad0
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-12-01 08:58:03 +0000

    Pycle

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongodb+srv://Swae:<Innovator@12>@cluster0.m9urv.mongodb.net/amazona?retryWrites=true&w=majority
-
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
