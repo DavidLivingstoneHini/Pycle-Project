42caeaae33dc57e6152ea5bfb9b8a71a8f63348a
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-12-04 10:38:42 +0000

    Pycle

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken';
-import mg from 'mailgun-js';
+import jwt from "jsonwebtoken";
+import mg from "mailgun-js";
 
 export const generateToken = (user) => {
   return jwt.sign(
@@ -10,9 +10,9 @@ export const generateToken = (user) => {
       isAdmin: user.isAdmin,
       isSeller: user.isSeller,
     },
-    process.env.JWT_SECRET || 'somethingsecret',
+    process.env.JWT_SECRET || "somethingsecret",
     {
-      expiresIn: '30d',
+      expiresIn: "30d",
     }
   );
 };
@@ -23,10 +23,10 @@ export const isAuth = (req, res, next) => {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(
       token,
-      process.env.JWT_SECRET || 'somethingsecret',
+      process.env.JWT_SECRET || "somethingsecret",
       (err, decode) => {
         if (err) {
-          res.status(401).send({ message: 'Invalid Token' });
+          res.status(401).send({ message: "Invalid Token" });
         } else {
           req.user = decode;
           next();
@@ -34,28 +34,28 @@ export const isAuth = (req, res, next) => {
       }
     );
   } else {
-    res.status(401).send({ message: 'No Token' });
+    res.status(401).send({ message: "No Token" });
   }
 };
 export const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send({ message: 'Invalid Admin Token' });
+    res.status(401).send({ message: "Invalid Admin Token" });
   }
 };
 export const isSeller = (req, res, next) => {
   if (req.user && req.user.isSeller) {
     next();
   } else {
-    res.status(401).send({ message: 'Invalid Seller Token' });
+    res.status(401).send({ message: "Invalid Seller Token" });
   }
 };
 export const isSellerOrAdmin = (req, res, next) => {
   if (req.user && (req.user.isSeller || req.user.isAdmin)) {
     next();
   } else {
-    res.status(401).send({ message: 'Invalid Admin/Seller Token' });
+    res.status(401).send({ message: "Invalid Admin/Seller Token" });
   }
 };
 
@@ -89,7 +89,7 @@ export const payOrderEmailTemplate = (order) => {
     </tr>
   `
     )
-    .join('\n')}
+    .join("\n")}
   </tbody>
   <tfoot>
   <tr>
