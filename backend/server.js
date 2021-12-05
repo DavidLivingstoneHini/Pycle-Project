7e5a7baefd33b7c78b0958313d5aab4283d0be44
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-12-05 12:01:50 +0000

    Pycle

--- backend/server.js
+++ backend/server.js
@@ -47,10 +47,6 @@ app.get("*", (req, res) =>
 //   res.send('Server is ready');
 // });
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
-
 const port = process.env.PORT || 5000;
 
 const httpServer = http.Server(app);
