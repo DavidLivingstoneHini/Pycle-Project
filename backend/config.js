6ee1158ef8c89242e5f2a317c10779e1464f2e35
DavidLivingstoneHini <kwamelivingstone77@gmail.com>
2021-12-06 18:20:31 +0000

    Pycle

---
+++ backend/config.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL:
+    process.env.MONGODB_URL ||
+    "mongodb+srv://Swae:Innovator%4012@cluster0.m9urv.mongodb.net/amazona?retryWrites=true&w=majority",
+  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "No-GOOGLE_KEY",
+  AMAZON_ACCESS_KEY_ID: process.env.AMAZON_ACCESS_KEY_ID || "NO_AMAZON_ID",
+  AMAZON_SECRET_ACCESS_KEY:
+    process.env.AMAZON_SECRET_ACCESS_KEY || "NO_AMAZON_KEY",
+};
--- backend/utils.js
+++ backend/utils.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import mg from "mailgun-js";
+import config from "./config.js";
 
-export const generateToken = (user) => {
+const getToken = (user) => {
   return jwt.sign(
     {
       _id: user._id,
@@ -10,120 +10,43 @@ export const generateToken = (user) => {
       isAdmin: user.isAdmin,
       isSeller: user.isSeller,
     },
-    process.env.JWT_SECRET || "somethingsecret",
+    config.JWT_SECRET,
     {
       expiresIn: "30d",
     }
   );
 };
 
-export const isAuth = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
-    jwt.verify(
-      token,
-      process.env.JWT_SECRET || "somethingsecret",
-      (err, decode) => {
-        if (err) {
-          res.status(401).send({ message: "Invalid Token" });
-        } else {
-          req.user = decode;
-          next();
-        }
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const onlyToken = token.slice(7, token.length);
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid Token" });
+      } else {
+        req.user = decode;
+        next();
       }
-    );
+    });
   } else {
     res.status(401).send({ message: "No Token" });
   }
 };
-export const isAdmin = (req, res, next) => {
+
+const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401).send({ message: "Invalid Admin Token" });
+    return next();
   }
+  return res.status(401).send({ message: "Invalid Admin Token" });
 };
-export const isSeller = (req, res, next) => {
+
+const isSeller = (req, res, next) => {
   if (req.user && req.user.isSeller) {
-    next();
-  } else {
-    res.status(401).send({ message: "Invalid Seller Token" });
+    return next();
   }
-};
-export const isSellerOrAdmin = (req, res, next) => {
-  if (req.user && (req.user.isSeller || req.user.isAdmin)) {
-    next();
-  } else {
-    res.status(401).send({ message: "Invalid Admin/Seller Token" });
-  }
+  return res.status(401).send({ message: "Invalid Seller Token" });
 };
 
-export const mailgun = () =>
-  mg({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMIAN,
-  });
-
-export const payOrderEmailTemplate = (order) => {
-  return `<h1>Thanks for shopping with us</h1>
-  <p>
-  Hi ${order.user.name},</p>
-  <p>We have finished processing your order.</p>
-  <h2>[Order ${order._id}] (${order.createdAt.toString().substring(0, 10)})</h2>
-  <table>
-  <thead>
-  <tr>
-  <td><strong>Product</strong></td>
-  <td><strong>Quantity</strong></td>
-  <td><strong align="right">Price</strong></td>
-  </thead>
-  <tbody>
-  ${order.orderItems
-    .map(
-      (item) => `
-    <tr>
-    <td>${item.name}</td>
-    <td align="center">${item.qty}</td>
-    <td align="right"> GH₵${item.price.toFixed(2)}</td>
-    </tr>
-  `
-    )
-    .join("\n")}
-  </tbody>
-  <tfoot>
-  <tr>
-  <td colspan="2">Items Price:</td>
-  <td align="right"> GH₵${order.itemsPrice.toFixed(2)}</td>
-  </tr>
-  <tr>
-  <td colspan="2">Tax Price:</td>
-  <td align="right"> GH₵${order.taxPrice.toFixed(2)}</td>
-  </tr>
-  <tr>
-  <td colspan="2">Shipping Price:</td>
-  <td align="right"> GH₵${order.shippingPrice.toFixed(2)}</td>
-  </tr>
-  <tr>
-  <td colspan="2"><strong>Total Price:</strong></td>
-  <td align="right"><strong> GH₵${order.totalPrice.toFixed(2)}</strong></td>
-  </tr>
-  <tr>
-  <td colspan="2">Payment Method:</td>
-  <td align="right">${order.paymentMethod}</td>
-  </tr>
-  </table>
-  <h2>Shipping address</h2>
-  <p>
-  ${order.shippingAddress.fullName},<br/>
-  ${order.shippingAddress.address},<br/>
-  ${order.shippingAddress.city},<br/>
-  ${order.shippingAddress.country},<br/>
-  ${order.shippingAddress.postalCode}<br/>
-  </p>
-  <hr/>
-  <p>
-  Thanks for shopping with us.
-  </p>
-  `;
-};
+export { getToken, isAuth, isAdmin, isSeller };
